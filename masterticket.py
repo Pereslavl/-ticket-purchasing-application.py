e887bb2f1f3b02131103156f12a494ab50e03ff2
Sergey Kryukov <cydkom@gmail.com>
2018-12-23 01:11:51 +0300

    My version Proceed Y/N

--- masterticket.py
+++ masterticket.py
@@ -17,3 +17,20 @@ amount_due = num_tickets * TICKET_PRICE
 
 # Output on the screen result
 print("The total due is $ {}".format(amount_due))
+
+
+# Prompt to user if they want to proceed. Y/N
+question = input("Do you want to proceed?")
+
+# if they want to proceed
+if question = "y":
+    print("SOLD!, to confirm purchase")
+     # print out to the screen "SOLD!" to confirm purchase
+else:
+    print("Buy!")
+ # and then decrement the tickets remaining by the number of tickets purchased
+residue = tickets_remaining - amount_due
+print(residue)
+# Otherwise...
+
+     # Thanks them by name
